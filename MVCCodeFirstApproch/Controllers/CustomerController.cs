f15ebfaa72ce36fa5d1a14a6fcd9640799ae910d
aayush-aryan <log2aayusharyan@gmail.com>
2022-01-21 20:29:08 +0530

    [aayush-aryan]added. A customer model class and creating table of Customer model class by using EntityFrameWork and adding data in customer table by using sqlServerManagement Studio

---
+++ MVCCodeFirstApproch/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCCodeFirstApproch.Models;
+
+
+namespace MVCCodeFirstApproch.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext dbContext;
+
+        public CustomerController(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        //
+        public IActionResult Index()
+        {
+            var customers = dbContext.customers.ToList();
+            return View(customers); ;
+        }
+    }
+}
