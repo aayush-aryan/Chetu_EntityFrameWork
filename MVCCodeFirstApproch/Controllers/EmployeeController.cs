92113215fa22d99a2053b097df230c330825441e
aayush-aryan <log2aayusharyan@gmail.com>
2022-01-31 03:09:25 +0530

    [aayush]modified. adding employee details in form and saving employee data in database

--- MVCCodeFirstApproch/Controllers/EmployeeController.cs
+++ MVCCodeFirstApproch/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCCodeFirstApproch.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,18 +9,43 @@ namespace MVCCodeFirstApproch.Controllers
 {
     public class EmployeeController : Controller
     {
+        //to inject Employee View page (index.cshtml) or to inject Employee Service class in our conroller
+        //for showing emplyee dynamic data in our view page
+        private ApplicationDbContext dbContext; //dbContext is the property name
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+       // public IActionResult Index()
+       // {
+       //     var emps = new List<string>() { "Rahul", "Deepak", "John", "Ajay" };
+        //    ViewBag.employees = emps;
+        //    return View();
+       // }
+
         public IActionResult Index()
         {
-            var emps = new List<string>() { "Rahul", "Deepak", "John", "Ajay" };
-            ViewBag.employees = emps;
-            return View();
+            var emps = dbContext.Employees.ToList();
+            return View(emps);
         }
-
+        // for data loading by default HttpGet method be call by this method;
         public IActionResult Create()
         {
             return View();
         }
 
+        //for data submit Post method is used
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            //for adding data in database; (in employee model class)
+            dbContext.Employees.Add(employee);
+            // for saving data in database;
+            dbContext.SaveChanges();
+            // for redirecting data to view page which is Index.cshtml and action method be Index();
+            return RedirectToAction("Index");
+        }
         public IActionResult Details()
         {
             return View();
